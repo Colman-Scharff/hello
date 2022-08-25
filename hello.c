4d9292e157da9b7bb8aa1e31508bc317b799abac
Colman Scharff <cscharff3@huskers.unl.edu>
2022-08-25 13:27:39 +0000

    initial commit

---
+++ hello.c
@@ -0,0 +1,16 @@
+/*
+*   Author: Colman Scharff
+*   Date: 2022/08/25
+*
+*   Prints author's name
+*/
+
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+    printf("Colman Scharff");
+
+    return 0;
+}
